530120aef70f85fd842231d9b906c701bf51ea6e
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-26 16:26:45 -0500

    displaying heroes on dom

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,29 +4,43 @@ var myApp = angular.module("myApp", []);
 
 // initialize controller to work with html
 myApp.controller('heroController', ['$scope', '$http', function ($scope, $http) {
-  console.log('using ng');
-  $scope.getHeroes = function () {
-    console.log('in getHeroes function');
-  var newHero = {
-    alias: $scope.name,
-    first_name: $scope.first_name,
-    last_name: $scope.last_name,
-    city: $scope.city,
-    power_name: $scope.power_name
-  }; // end newHero
+  console.log('using heroController');
 
-// set up http call to retrieve heroes in database
-$http({
-  method: 'GET',
-  url: '/heroes'
-}).then( function (err, response){
-    if (err) {
-      console.log("http err:", err);
-    } else {
-      console.log('http success:', response);
-      var data = response.data;
-      console.log(data);
-    } // end else
-}); // end $http
+  // set up http call to retrieve heroes in database
+  $scope.getHeroes = function () {
+  $http({
+    method: 'GET',
+    url: '/heroes'
+  }).then( function (response){
+        console.log('http success:', response);
+        $scope.heroes = response.data;
+      }, function (error) {
+        console.log('error ;', error);
+      }); // end then function
 }; // end getHeroes function
+
+//   $scope.addHero = function () {
+//     console.log('in getHeroes function');
+//
+//   var newHero = {
+//     alias: $scope.name,
+//     first_name: $scope.first_name,
+//     last_name: $scope.last_name,
+//     city: $scope.city,
+//     power_name: $scope.power_name
+//   }; // end newHero
+//
+// set up http call to retrieve heroes in database
+// $http({
+//   method: 'POST',
+//   url: '/heroes',
+//   data: newHero
+// }).then( function (err, response){
+//     if (err) {
+//       console.log("http err:", err);
+//     } else {
+//       console.log('http success:', response);
+//     } // end else
+// }); // end $http POST
+// }; // end getHeroes function
 }]); // end controller
